85a86f0990933d9ccb6ba356b4ea2587fb8fb48d
Ali Ünsal <guvejokuaali@gmail.com>
2018-06-07 21:51:35 +0300

    07.06.2018

---
+++ knght/knight.c
@@ -0,0 +1,25 @@
+#include <16f877a.h>
+
+#fuses XT,NOWDT,NOPROTECT,NOBROWNOUT,NOLVP,NOPUT,NOWRT,NODEBUG,NOCPD
+#use delay(clock=4M)
+
+const int led[] = {pin_b0, pin_b1, pin_b2, pin_b3, pin_b4, pin_b5, pin_b6, pin_b7};
+void main()
+{
+   int i;
+   while(1)
+   {
+      i = 0;
+      for(i = 0; i < 8; i++) {
+         output_high(led[i]);
+         delay_ms(30);
+         output_low(led[i]);
+      }
+      for(i = 7; i >= 0; i--) {
+         output_high(led[i]);
+         delay_ms(30);
+         output_low(led[i]);
+      }
+   }
+
+}
